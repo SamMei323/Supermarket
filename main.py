850fa1ce1b5caaad63ddaeb211716dbe134c24df
SamMei323 <samuel19970901@gmail.com>
2020-02-25 02:01:10 +1300

    completed main scraping part for paknsave so need to implement for all pages and all categories

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from urllib.request import Request, urlopen
+import ast
 
-# driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
-
-names = []
-prices = []
+# initialize empty lists to store data
+productName = []
+unitPrice = []
 category = []
+image = []
+pricingUnit=[]
 
-# driver.get("https://www.paknsaveonline.co.nz/category/personal-care")
 req = Request("https://www.paknsaveonline.co.nz/category/personal-care", headers={'User-Agent': 'Mozilla/5.0'})
 content = urlopen(req)
 soup = BeautifulSoup(content,features="lxml")
-cnt = 1
-for a in soup.findAll('div',attrs={'class':'fs-product-card'}):
-    
-    names.append(a.find('h3',attrs={'class':"u-p2"}).text)
 
-print(names)
+print("start")
+for a in soup.findAll('div',attrs={'class':'fs-product-card'}):
+    card = a.find('div',attrs={'class','js-product-card-footer fs-product-card__footer-container'})
+    productDetails= card['data-options']
+    productDetails = productDetails.replace("\r","") ;productDetails = productDetails.replace("\n","");productDetails = productDetails.replace(" ","");productDetails = productDetails.replace("false","0")
+    productDetails = eval(productDetails)['ProductDetails']
+
+    unitPrice.append(productDetails["PricePerItem"])
+    productName.append(a.find('h3',attrs={'class':"u-p2"}).text.replace('\n','').replace('\r',''))
+    pricingUnit.append(productDetails['PriceMode'])
+    image.append(a.find('div',attrs={'class','fs-product-card__product-image'})['style'])
+print(pd.DataFrame({'productName':productName,'pricingUnit':pricingUnit,'unitPrice':unitPrice}))
