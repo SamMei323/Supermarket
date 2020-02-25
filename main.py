be236c75e842ff3af4452fc78b11aee41ec260c9
SamMei323 <samuel19970901@gmail.com>
2020-02-25 17:02:11 +1300

    finished paknsave-pre testing

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
-from selenium import webdriver
 from urllib.request import Request, urlopen
 import ast
+import os
+
+# create list of paknsave catergories
+categories = ['drinks','fresh-foods-and-bakery','chilled-frozen-and-desserts','pantry','drinks','beer-cider-and-wine','personal-care','baby-toddler-and-kids','pets','kitchen-dining-and-household']
 
 # initialize empty lists to store data
 productName = []
 unitPrice = []
 category = []
 image = []
-pricingUnit=[]
+pricingUnit= []
+productId = []
 
-req = Request("https://www.paknsaveonline.co.nz/category/personal-care", headers={'User-Agent': 'Mozilla/5.0'})
-content = urlopen(req)
-soup = BeautifulSoup(content,features="lxml")
+for cat in categories:
+###
+    print(cat)
+###
+    req = Request("https://www.paknsaveonline.co.nz/category/"+cat, headers={'User-Agent': 'Mozilla/5.0'})
+    content = urlopen(req)
+    soup = BeautifulSoup(content,features="lxml")
+    numPages = (int(soup.findAll('a', attrs={'class':"btn btn--tertiary btn--large fs-pagination__btn"})[-1].text))
+    
+    for pageNum in range(1,numPages+1):
+        req = Request("https://www.paknsaveonline.co.nz/category/"+cat+'?pg='+str(pageNum), headers={'User-Agent': 'Mozilla/5.0'})
+        content = urlopen(req)
+        soup = BeautifulSoup(content,features="lxml")
+###
+        if(pageNum%5 == 0 or pageNum==1):
+            print(pageNum)
+###
+        for a in soup.findAll('div',attrs={'class':'fs-product-card'}):
+            card = a.find('div',attrs={'class','js-product-card-footer fs-product-card__footer-container'})
+            dataOptions= card['data-options'] # note here dataOptions is a bs4 tag object/dictionary that contains information about product
+            dataOptions = dataOptions.replace("\r","").replace("\n","").replace(" ","").replace("false","0").replace("true","0")
+            productDetails = eval(dataOptions)['ProductDetails']
 
-print("start")
-for a in soup.findAll('div',attrs={'class':'fs-product-card'}):
-    card = a.find('div',attrs={'class','js-product-card-footer fs-product-card__footer-container'})
-    productDetails= card['data-options']
-    productDetails = productDetails.replace("\r","") ;productDetails = productDetails.replace("\n","");productDetails = productDetails.replace(" ","");productDetails = productDetails.replace("false","0")
-    productDetails = eval(productDetails)['ProductDetails']
+            productId.append(eval(dataOptions)['productId'])
+            unitPrice.append(float(productDetails["PricePerItem"]))
+            productName.append(a.find('h3',attrs={'class':"u-p2"}).text.replace('\n','').replace('\r',''))
+            pricingUnit.append(productDetails['PriceMode'])
+            image.append(a.find('div',attrs={'class','fs-product-card__product-image'})['style'])
 
-    unitPrice.append(productDetails["PricePerItem"])
-    productName.append(a.find('h3',attrs={'class':"u-p2"}).text.replace('\n','').replace('\r',''))
-    pricingUnit.append(productDetails['PriceMode'])
-    image.append(a.find('div',attrs={'class','fs-product-card__product-image'})['style'])
-print(pd.DataFrame({'productName':productName,'pricingUnit':pricingUnit,'unitPrice':unitPrice}))
+df = pd.DataFrame({'productName':productName,'pricingUnit':pricingUnit,'unitPrice':unitPrice})
+
+print(df.head())
+print(df.tail())
+df.to_csv(os.path.expanduser("~/Desktop/SupermarketData/paknsave_allproducts.csv"),index=False)
