691f3a259e32e7479f52ecdb16e649a111baf7a6
SamMei323 <samuel19970901@gmail.com>
2020-02-27 04:22:53 +1300

    done

--- countdown.py
+++ countdown.py
@@ -18,26 +18,50 @@ category = []
 image = []
 pricingUnit= []
 productId = []
-quantity = []
+packageType= []
+volumeSize = []
+categories = ['bakery','deli-chilled-foods','easter','fruit-vegetables','meat','seafood',
+'baby-care','baking-cooking','biscuits-crackers','breakfast-foods','canned-prepared-foods',
+'chocolate-sweets-snacks','cleaning-homecare','clothing-manchester','drinks-hot-cold','frozen-foods','health-wellness','home-kitchenware',
+'liquor-beer-cider','liquor-wine','meal-ingredients','office-entertainment','personal-care','pet-care','toys-party-needs']
 
 
-driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get("https://shop.countdown.co.nz/shop/browse/bakery?page=1")
-content = driver.page_source
 
-soup = BeautifulSoup(content,features="lxml")
+for cat in categories:
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+    driver.get("https://shop.countdown.co.nz/shop/browse/"+cat)
+    content = driver.page_source
+    soup = BeautifulSoup(content,features="lxml")
+    pages = []
+    for a in soup.findAll('li',attrs={'class':'page-number'}):
+        pages.append((int(a.find('a').text)))
+    numPages = max(pages);pages=None
+    driver.quit()
 
-productGrid = soup.find('product-grid-element',id='grid-0')
-# print(productGrid)
-products = productGrid['products'].replace("null",'''"null"''').replace('false','0').replace('true','0')
-products = eval(products)
-for product in products:
-    print(product['name'])
+    for pageNum in range(1,numPages+1):
+        driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+        driver.get("https://shop.countdown.co.nz/shop/browse/"+cat+'?page='+str(pageNum))
+        content = driver.page_source
+        soup = BeautifulSoup(content,features="lxml")
+        productGrid = soup.find('product-grid-element',id='grid-0')
+        # print(productGrid)
+        products = productGrid['products'].replace("null",'''"null"''').replace('false','False').replace('true','True')
+        products = eval(products)
 
-# for product in soup.findAll('product-grid-element',attrs={'id':'grid-0'}):
-#     print(product)
-#     print(product.find('h2 _ngcontent-product-c3',attrs={'id':"product-0-title"}))
+        for product in products:
+            category.append(cat)
+            productName.append(product['name'])
+            productId.append(product['sku'])
+            unitPrice.append(float(product['price']['salePrice']))
+            image.append("https://shop.countdown.co.nz/"+product['images']['big'])
+            pricingUnit.append(product['unit'])
+            packageType.append(product['size']['packageType'])
+            volumeSize.append(product['size']['packageType'])
+        driver.quit()
 
-driver.quit()
+    df = pd.DataFrame({'category':category,'productId':productId,'productName':productName,'packageType':packageType,'volumeSize':volumeSize,'pricingUnit':pricingUnit,'unitPrice':unitPrice,'image':image})
+    df.to_csv(os.path.expanduser("~/Desktop/SupermarketData/Countdown/countdown_"+cat+".csv"),index=False)
+
+# driver.quit()
 ###########
 print("\nCountdown Total Execution Time: " + str((time.time()-t0)/60) + " minutes")
